d0aa9059d8d92e3da1796f01946260afe7ad9254
Chen Kar Xuan <chenkarxuan@gmail.com>
2025-03-14 12:29:20 +0800

    Home page done

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,19 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Aloysius';
+export const SITE_DESCRIPTION = 'My Portfolio Blog!';
+export const menuLinks: Array<{ title: string; path: string }> = [
+	{
+		title: "Home",
+		path: "/",
+	},
+	{
+		title: "Blog",
+		path: "/blog/",
+	},
+	{
+		title: "About",
+		path: "/about/",
+	},
+];
